7443f299f6a6097fe8fab4d68d6665b402e8f8c4
samyak <samyakchheda000@gmail.com>
2024-11-30 15:55:47 +0530

    made wsgi.py and index.py

--- app.py
+++ app.py
@@ -77,6 +77,4 @@ def predict_behavior():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
+
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from app import app
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
