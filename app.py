98fdad7ee5c75d467e1e6fc7031dd10f8b546015
rudraparmar76 <rudraparmar1309@gmail.com>
2024-12-01 13:34:14 +0530

    major changes

--- app.py
+++ app.py
@@ -103,3 +103,5 @@ def predict_behavior():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+if __name__ == '__main__':
+    app.run(debug=True)
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from app import app
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
